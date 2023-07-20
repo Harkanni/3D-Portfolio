fe483096992b6ccbfbc9d384fcecb8396363fc36
harkanni <harkanzyempire001@gmail.com>
2023-07-20 13:33:35 +0100

    initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,6 +14,8 @@ const Contact = () => {
     email: '',
     message: ''
   });
+  const formRef = useRef()
+
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {};
@@ -25,6 +27,18 @@ const Contact = () => {
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.heroHeadText}>Contact.</h3>
+
+        <form action="" ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your name</span>
+            <input type="text" name='name' value={form.name} onChange={handleChange} placeholder="What's your name?" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <input type="email" name='email' value={form.email} onChange={handleChange} placeholder="What's your email?" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+          </label>
+        </form>
       </motion.div>
     </div>
   );
